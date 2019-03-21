115af883d92b08e12f1f95ffe7d7b3c9e2226062
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2019-03-20 23:14:14 -0300

    mixing some stuff, add heroku build

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    state = {
+        contents: []
+    }
+
+    componentDidMount() {
+        this.getContents();
+    }
+
+    getContents = () => {
+        fetch('/api/v1/contents')
+            .then(res => res.json())
+            .then(({contents}) => this.setState({contents}));
+    }
+
     render() {
+        const { contents } = this.state;
+
         return (
             <div>
                 <h1>Hello Leito Wiki</h1>
+                {contents.map((content, index) => (
+                    <h2 key={index}>{content.title}</h2>
+                ))}
             </div>
         );
     }
