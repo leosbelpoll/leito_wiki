eae0693c3f663d79d7d66a66eeee47dd7f369ff6
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2019-03-20 22:30:49 -0300

    manage client routes

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use('/api/v1/contents', content);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 module.exports = app;
