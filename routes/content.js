2add747288fefb1a34373651a89969b1f5b20eac
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2019-03-20 22:21:26 -0300

    just for test heroku

--- routes/content.js
+++ routes/content.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 /* GET contents listing. */
 router.get('/', function(req, res, next) {
-  res.json({contents: [{title: 'Linux'}, {title: 'Debian'}]});
+  res.json({
+    contents: [
+      {title: 'Linux'},
+      {title: 'Debian'},
+      {title: 'Installation'},
+    ]
+  });
 });
 
 module.exports = router;
