b154c633760bb02157ea3469e645295bc76fc964
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2019-03-22 00:13:06 -0300

    POST PUT in content postgres

--- routes/content.js
+++ routes/content.js
@@ -22,4 +22,49 @@ router.get('/', function(req, res, next) {
         });
 });
 
+/* POST contents listing. */
+router.post('/', function(req, res, next) {
+    const { content } = req.body;
+    db.query('INSERT INTO contents (${this:name}) VALUES(${this:csv})', content)
+        .then(function (content) {
+            res.json({
+                content
+            });
+        })
+        .catch(function (error) {
+            console.error(error);
+            res.status(500).send(error);
+        });
+});
+
+/* PUT contents listing. */
+router.put('/', function(req, res, next) {
+    const { content: { id, title, content } } = req.body;
+    db.query('UPDATE contents SET title = $2, content = $3 WHERE id = $1', [id, title, content])
+        .then(function (content) {
+            res.json({
+                content
+            });
+        })
+        .catch(function (error) {
+            console.error(error);
+            res.status(500).send(error);
+        });
+    
+});
+
+/* GET ALL contents listing. */
+router.get('/all', function(req, res, next) {
+    db.query('SELECT * FROM contents')
+        .then(function (contents) {
+            res.json({
+                contents
+            });
+        })
+        .catch(function (error) {
+            console.error(error);
+            res.status(500).send(error);
+        });
+});
+
 module.exports = router;
